b671e1bb126df80e5e42b668dcc84db992f1f9ba
Pavel07-chyden <pates1993@mail.ru>
2022-03-08 23:20:20 +0300

    info

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(function () {
    $('.design-slider').slick({
-      dots: false ,
+      dots: false,
       slidesToShow: 4,
       variableWidth: true,
       prevArrow: '<img class="arrow arror-left" src="./images/arrow-left.svg" alt="">',
       nextArrow: '<img class="arrow arror-right" src="./images/arrow-rigth.svg" alt="">',
       responsive: [
          {
-            breakpoint: 361, 
+            breakpoint: 361,
             setting: {
                variableWidth: false,
                slidesToShow: 1,
@@ -17,3 +17,4 @@ $(function () {
 
    })
 });
+//  <div class="video">
